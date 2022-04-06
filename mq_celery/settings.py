197722d4f2b28983606a381fb858c6d79292f618
Jungbum Woo <jungbumwoo@naver.com>
2022-04-06 22:41:26 +0900

    celery activate

--- mq_celery/settings.py
+++ mq_celery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'app1'
             ],
         },
     },
